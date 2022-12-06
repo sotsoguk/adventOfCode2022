432c494cf319f547008462bf2e45f21797d0090d
Bert Riffelmacher <sotsoguk@gmail.com>
2022-12-06 15:13:27 +0100

    [Day01] One-Liner

--- python/day01/day01.py
+++ python/day01/day01.py
@@ -1,8 +1,10 @@
 import os
 import time
+import heapq
 
 
 def main():
+
     # input
     print(os.getcwd())
     day = "01"
@@ -11,29 +13,17 @@ def main():
     print(input_file)
     part1, part2 = 0, 0
     with open(input_file) as f:
-        lines = f.read().splitlines()
-
-    # parse input
-    calories = []
-    curr_calories = []
-    for l in lines:
-        if l == "":
-            calories.append(curr_calories.copy())
-            curr_calories = []
-        else:
-            curr_calories.append(int(l, base=10))
-    calories.append(curr_calories)
+        groups = f.read().split('\n\n')
 
     start_time = time.time()
+    # calc
+    calories = sorted([sum(list(map(int, group.splitlines())))
+                      for group in groups])
+    part1 = calories[-1]
+    part2 = sum(calories[-3:])
 
-    # part1 & 2
-    total_cals = [sum(c) for c in calories]
-
-    part1 = max(total_cals)
-    part2 = sum(sorted(total_cals)[-3:])
-
+    # output
     duration = int((time.time() - start_time) * 1000000)
-
     header = "#" * 20
     print(
         f"{header}\n*AoC {year} - Day {day} *\n{header}\n\nPart 1:\t{part1}\nPart 2:\t{part2}\nTime:\t{duration // 1000} ms")
