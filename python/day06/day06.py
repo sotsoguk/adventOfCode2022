de4b9a4382f403c7ec8c21c22bd0da8c9428e7da
Bert Riffelmacher <sotsoguk@gmail.com>
2022-12-06 15:13:57 +0100

    Misc

--- python/day06/day06.py
+++ python/day06/day06.py
@@ -2,6 +2,9 @@ import os
 import time
 from string import ascii_lowercase
 from collections import defaultdict
+import timeit
+
+inputt = ""
 
 
 def first_all_unique(input, window_size=4):
@@ -10,8 +13,13 @@ def first_all_unique(input, window_size=4):
         char_dict[char] += 1
         if i >= window_size:
             char_dict[input[i-window_size]] -= 1
-            if all(v <= 1 for v in char_dict.values()):
+            # if all(v <= 1 for v in char_dict.values()):
+            #     return i+1
+            if any(v > 1 for v in char_dict.values()):
+                continue
+            else:
                 return i+1
+
     # no position found
     return -1
 
@@ -29,10 +37,23 @@ def main():
 
     start_time = time.time()
     input = lines[0]
-
+    inputt = lines[0]
     part1 = first_all_unique(input, window_size=4)
-    part2 = first_all_unique(input, window_size=14)
+    for i in range(2000):
+        part2 = first_all_unique(input, window_size=14)
     duration = int((time.time() - start_time) * 1000000)
+    # benchmark
+    stmt_code = "first_all_unique(inputt,14)"
+    # setup_code = """
+    # from __main__ import first_all_unique
+    # from __main__ import input
+    # """
+    setup_code = """
+from __main__ import first_all_unique
+input = inputt
+    """  # , input"
+    # iterations = 10000
+    # print(timeit.timeit(stmt=stmt_code,setup=setup_code,number= iterations,globals=globals())/iterations*1000000)
     header = "#" * 21
     print(
         f"{header}\n* AoC {year} - Day {day} *\n{header}\n\nPart 1:\t{part1}\nPart 2:\t{part2}\nTime:\t{duration // 1000} ms")
